f181ccd860ed39f9d1b89507cd313a74bde68f9f
James Cliff <me@jamescliff.com>
2022-03-01 13:11:22 -0500

    fix: updates

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
 import Head from "next/head";
-import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Stars } from "@react-three/drei";
+import { Suspense, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, Stars, Sphere, useTexture } from "@react-three/drei";
 import Logo from "./components/Logo.js";
-import Planet from "./components/Planet.js";
+import { LayerMaterial, Base, Noise, Depth, Fresnel, Texture } from "lamina";
+
+const Planet = () => {
+  const ref = useRef();
+  useFrame(({ clock }) => {
+    if (ref.current) {
+      ref.current.rotation.y = clock.getElapsedTime() / 1.6;
+    }
+  });
+  return (
+    <Sphere ref={ref} scale={2}>
+      <ambientLight intensity={1} />
+      <pointLight position={[100, 100, 100]} />
+      <LayerMaterial>
+        <Base color="#D33CE7" valpha={1} mode="normal" />
+        <Noise colorA="#5B2CCB" colorB="#000000" alpha={1} mode="lighten" />
+        <Texture map={useTexture("/volcanic.png")} alpha={0.75} />
+        {/* <Texture map={texture[1]} alpha={0.5} /> */}
+      </LayerMaterial>
+    </Sphere>
+  );
+};
 
 export default function Home() {
   return (
